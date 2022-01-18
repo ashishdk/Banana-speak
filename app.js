9b0faf5044e1a68feeb69618c244d70879f127e2
ashishdk <ashish.dewade@gmail.com>
2022-01-18 08:56:35 +0530

    initialise

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-console.log("HI, This is from external source.")
-var username=prompt("What are you doing here?");
-var welmsg="good you are learning!! " +username;
-alert(welmsg);
+var btntranslate=document.querySelector("#btn-translate");
+
+btntranslate.addEventListener("click", clickEventhandler);
+
+function clickEventhandler(){
+    console.log("clickeddd");
+} 
