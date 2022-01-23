8519a47873f18e88c825b5e4ee04fc834f8654e2
ashishdk <ashish.dewade@gmail.com>
2022-01-23 17:11:31 +0530

    done coding

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ var btntranslate=document.querySelector("#btn-translate");
 var txtinput=document.querySelector("#txt-input");
 var outputdiv=document.querySelector("#output");
 
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL="https://api.funtranslations.com/translate/yoda.json"; // Yoda translation from star wars
+//var serverURL="https://api.funtranslations.com/translate/minion.json"; // Minion
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text
+}
+
+function errorHandler(){
+    console.log("error occured" + error);
+    }
 
 function clickEventhandler(){
-    outputdiv.innerText="asdhbadb"+ txtinput.value ;   
+    var inputText= txtinput.value;
+        fetch(getTranslationURL(inputText))
+        .then(response=>response.json())
+            .then(json=>
+            {
+            var TranslatedTxt= json.contents.translated;
+            outputdiv.innerText=TranslatedTxt;
+        }
+        )
+    .cache(errorHandler)
 }
 
 btntranslate.addEventListener("click", clickEventhandler);
